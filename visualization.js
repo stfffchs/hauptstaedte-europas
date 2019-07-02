1b661f5e09698692246e8763e2597789642a2459
stfffchs <st.a.fuchs@gmail.com>
2019-07-02 20:12:27 +0200

    first one

--- visualization.js
+++ visualization.js
@@ -1,32 +1,65 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
+  // Definiere die Breite (width) und Höhe (height) deiner Visualisierung `800x600`
+  const width = 800
+  const height = 600
+
+  // Hole den #viz container aus dem DOM mit `d3.select()`
+  const container = d3.select("#viz")
+    .attr("fill", "blue")
+
+
+  // Erstelle eine SVG node mit den Dimensionen `800x600`
+  const svg = container.append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .style("border", "1px solid #000")
+
+  // Geografische Formen sind in `countries.features`
+
   d3.json("/countries.json")
     .then(countries => {
+
+
+      // Konfiguriere eine Kartenprojektion
+      const projection = d3.geoBonne()
+        .rotate([-20.0, 0.0])
+        .center([0.0, 52.0])
+        .parallel(52)
+        .translate([width / 2, height / 2])
+        .scale(700)
+        .precision(.1)
+
+      // Erstelle einene Pfadgenerator mit `d3.geoPath()`
+      const path = d3.geoPath().projection(projection)
+
+
+      // Optional: Erstelle ein Gradnetz mit `d3.geoGraticule()()`
+
+      // Binde `countries.features` an Pfade
+      const laender = svg.selectAll("path")
+        .data(countries.features)
+        .enter()
+        .append("path")
+        .attr("d", d => path(d))
+
+
+      // Städtedaten sind in `cities`
+
       d3.csv("/cities.csv")
         .then(cities => {
 
-          // Geografische Formen sind in `countries.features`
-
-          // Städtedaten sind in `cities`
-          
-          // ========================================
-
-          // Definiere die Breite (width) und Höhe (height) deiner Visualisierung `800x600`
-
-          // Hole den #viz container aus dem DOM mit `d3.select()`
-
-          // Erstelle eine SVG node mit den Dimensionen `800x600`
-    
-          // Konfiguriere eine Kartenprojektion
-    
-          // Erstelle einene Pfadgenerator mit `d3.geoPath()`
-
-          // Optional: Erstelle ein Gradnetz mit `d3.geoGraticule()()`
-
-          // Binde `countries.features` an Pfade
-    
           // Binde `cities` an Kreise
+          const cirlces = svg.selectAll('circle')
+            .data(cities)
+            .enter()
+            .append('circle')
+            .attr("cx", d => projection([d.lon, d.lat])[0])
+            .attr("cy", d => projection([d.lon, d.lat])[1])
+            .attr("r", d => d.size)
+            .attr('fill', 'rgba(255, 100, 0, 0.5)');
+
 
           // ========================================
 
